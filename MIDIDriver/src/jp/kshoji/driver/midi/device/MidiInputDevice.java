62e044a6321054bf1181e11ff026781632ec87e2
Kaoru Shoji <0x0badc0de@gmail.com>
2014-03-24 20:08:38 +0900

    Reduce memory allocation at MidiInputDevice.

--- MIDIDriver/src/jp/kshoji/driver/midi/device/MidiInputDevice.java
+++ MIDIDriver/src/jp/kshoji/driver/midi/device/MidiInputDevice.java
@@ -1,8 +1,7 @@
 package jp.kshoji.driver.midi.device;
 
-import java.io.ByteArrayOutputStream;
-
 import jp.kshoji.driver.midi.listener.OnMidiInputEventListener;
+import jp.kshoji.driver.midi.util.ReusableByteArrayOutputStream;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbEndpoint;
@@ -148,173 +147,169 @@ public final class MidiInputDevice {
 			byte[] readBuffer = new byte[maxPacketSize * 2]; // *2 for safety (BUFFER_LENGTH+4 would be enough)
 			int readBufferSize = 0;
 			byte[] read = new byte[maxPacketSize * 2];
-			ByteArrayOutputStream systemExclusive = null;
+			@SuppressWarnings("resource")
+			final ReusableByteArrayOutputStream systemExclusive = new ReusableByteArrayOutputStream();
+			int length;
+			int cable;
+			int codeIndexNumber;
+			int byte1;
+			int byte2;
+			int byte3;
+			int i;
 
+			// Don't allocate instances in the loop, as much as possible.
 			while (!stopFlag) {
-				int length = deviceConnection.bulkTransfer(usbEndpoint, bulkReadBuffer, maxPacketSize, 0);
-				if (length > 0) {
-					synchronized (suspendSignal) {
-						if (suspendFlag) {
-							try {
-								// check the deviceConnection to ignore events while suspending.
-								// Note: Events received within last sleeping(100msec) will be sent to the eventListener.
-								suspendSignal.wait(100);
-							} catch (InterruptedException e) {
-								// ignore exception
-							}
-							continue;
+				length = deviceConnection.bulkTransfer(usbEndpoint, bulkReadBuffer, maxPacketSize, 0);
+				
+				synchronized (suspendSignal) {
+					if (suspendFlag) {
+						try {
+							// check the deviceConnection to ignore events while suspending.
+							// Note: Events received within last sleeping(100msec) will be sent to the eventListener.
+							suspendSignal.wait(100);
+						} catch (InterruptedException e) {
+							// ignore exception
 						}
-					}
-					
-					System.arraycopy(bulkReadBuffer, 0, readBuffer, readBufferSize, length);
-					readBufferSize += length;
-
-					if (readBufferSize < 4) {
-						// more data needed
 						continue;
 					}
+				}
+				
+				if (length <= 0) {
+					continue;
+				}
+				
+				System.arraycopy(bulkReadBuffer, 0, readBuffer, readBufferSize, length);
+				readBufferSize += length;
 
-					// USB MIDI data stream: 4 bytes boundary
-					final int readSize = readBufferSize / 4 * 4;
-					System.arraycopy(readBuffer, 0, read, 0, readSize); // fill the read array
+				if (readBufferSize < 4) {
+					// more data needed
+					continue;
+				}
 
-					// keep unread bytes
-					int unreadSize = readBufferSize - readSize;
-					if (unreadSize > 0) {
-						System.arraycopy(readBuffer, readSize, readBuffer, 0, unreadSize);
-						readBufferSize = unreadSize;
-					} else {
-						readBufferSize = 0;
+				// USB MIDI data stream: 4 bytes boundary
+				final int readSize = readBufferSize / 4 * 4;
+				System.arraycopy(readBuffer, 0, read, 0, readSize); // fill the read array
+
+				// keep unread bytes
+				final int unreadSize = readBufferSize - readSize;
+				if (unreadSize > 0) {
+					System.arraycopy(readBuffer, readSize, readBuffer, 0, unreadSize);
+					readBufferSize = unreadSize;
+				} else {
+					readBufferSize = 0;
+				}
+
+				for (i = 0; i < readSize; i += 4) {
+					cable = (read[i + 0] >> 4) & 0xf;
+					codeIndexNumber = read[i + 0] & 0xf;
+					byte1 = read[i + 1] & 0xff;
+					byte2 = read[i + 2] & 0xff;
+					byte3 = read[i + 3] & 0xff;
+
+					switch (codeIndexNumber) {
+					case 0:
+						eventListener.onMidiMiscellaneousFunctionCodes(sender, cable, byte1, byte2, byte3);
+						break;
+					case 1:
+						eventListener.onMidiCableEvents(sender, cable, byte1, byte2, byte3);
+						break;
+					case 2:
+					// system common message with 2 bytes
+					{
+						byte[] bytes = new byte[] { (byte) byte1, (byte) byte2 };
+						eventListener.onMidiSystemCommonMessage(sender, cable, bytes);
 					}
-
-					int cable;
-					int codeIndexNumber;
-					int byte1;
-					int byte2;
-					int byte3;
-					for (int i = 0; i < readSize; i += 4) {
-						cable = (read[i + 0] >> 4) & 0xf;
-						codeIndexNumber = read[i + 0] & 0xf;
-						byte1 = read[i + 1] & 0xff;
-						byte2 = read[i + 2] & 0xff;
-						byte3 = read[i + 3] & 0xff;
-
-						switch (codeIndexNumber) {
-						case 0:
-							eventListener.onMidiMiscellaneousFunctionCodes(sender, cable, byte1, byte2, byte3);
-							break;
-						case 1:
-							eventListener.onMidiCableEvents(sender, cable, byte1, byte2, byte3);
-							break;
-						case 2:
-						// system common message with 2 bytes
-						{
-							byte[] bytes = new byte[] { (byte) byte1, (byte) byte2 };
-							eventListener.onMidiSystemCommonMessage(sender, cable, bytes);
+						break;
+					case 3:
+					// system common message with 3 bytes
+					{
+						byte[] bytes = new byte[] { (byte) byte1, (byte) byte2, (byte) byte3 };
+						eventListener.onMidiSystemCommonMessage(sender, cable, bytes);
+					}
+						break;
+					case 4:
+						// sysex starts, and has next
+						synchronized (systemExclusive) {
+							systemExclusive.write(byte1);
+							systemExclusive.write(byte2);
+							systemExclusive.write(byte3);
 						}
-							break;
-						case 3:
-						// system common message with 3 bytes
-						{
-							byte[] bytes = new byte[] { (byte) byte1, (byte) byte2, (byte) byte3 };
+						break;
+					case 5:
+						// system common message with 1byte
+						// sysex end with 1 byte
+						if (systemExclusive == null) {
+							byte[] bytes = new byte[] { (byte) byte1 };
 							eventListener.onMidiSystemCommonMessage(sender, cable, bytes);
-						}
-							break;
-						case 4:
-							// sysex starts, and has next
-							synchronized (this) {
-								if (systemExclusive == null) {
-									systemExclusive = new ByteArrayOutputStream();
-								}
+						} else {
+							synchronized (systemExclusive) {
+								systemExclusive.write(byte1);
+								eventListener.onMidiSystemExclusive(sender, cable, systemExclusive.toByteArray());
+								systemExclusive.reset();
 							}
+						}
+						break;
+					case 6:
+						// sysex end with 2 bytes
+						if (systemExclusive != null) {
+							synchronized (systemExclusive) {
+								systemExclusive.write(byte1);
+								systemExclusive.write(byte2);
+								eventListener.onMidiSystemExclusive(sender, cable, systemExclusive.toByteArray());
+								systemExclusive.reset();
+							}
+						}
+						break;
+					case 7:
+						// sysex end with 3 bytes
+						if (systemExclusive != null) {
 							synchronized (systemExclusive) {
 								systemExclusive.write(byte1);
 								systemExclusive.write(byte2);
 								systemExclusive.write(byte3);
+								eventListener.onMidiSystemExclusive(sender, cable, systemExclusive.toByteArray());
+								systemExclusive.reset();
 							}
-							break;
-						case 5:
-							// system common message with 1byte
-							// sysex end with 1 byte
-							if (systemExclusive == null) {
-								byte[] bytes = new byte[] { (byte) byte1 };
-								eventListener.onMidiSystemCommonMessage(sender, cable, bytes);
-							} else {
-								synchronized (systemExclusive) {
-									systemExclusive.write(byte1);
-									eventListener.onMidiSystemExclusive(sender, cable, systemExclusive.toByteArray());
-								}
-								synchronized (this) {
-									systemExclusive = null;
-								}
-							}
-							break;
-						case 6:
-							// sysex end with 2 bytes
-							if (systemExclusive != null) {
-								synchronized (systemExclusive) {
-									systemExclusive.write(byte1);
-									systemExclusive.write(byte2);
-									eventListener.onMidiSystemExclusive(sender, cable, systemExclusive.toByteArray());
-								}
-								synchronized (this) {
-									systemExclusive = null;
-								}
-							}
-							break;
-						case 7:
-							// sysex end with 3 bytes
-							if (systemExclusive != null) {
-								synchronized (systemExclusive) {
-									systemExclusive.write(byte1);
-									systemExclusive.write(byte2);
-									systemExclusive.write(byte3);
-									eventListener.onMidiSystemExclusive(sender, cable, systemExclusive.toByteArray());
-								}
-								synchronized (this) {
-									systemExclusive = null;
-								}
-							}
-							break;
-						case 8:
-							eventListener.onMidiNoteOff(sender, cable, byte1 & 0xf, byte2, byte3);
-							break;
-						case 9:
-							if (byte3 == 0x00) {
-								eventListener.onMidiNoteOff(sender, cable, byte1 & 0xf, byte2, byte3);
-							} else {
-								eventListener.onMidiNoteOn(sender, cable, byte1 & 0xf, byte2, byte3);
-							}
-							break;
-						case 10:
-							// poly key press
-							eventListener.onMidiPolyphonicAftertouch(sender, cable, byte1 & 0xf, byte2, byte3);
-							break;
-						case 11:
-							// control change
-							eventListener.onMidiControlChange(sender, cable, byte1 & 0xf, byte2, byte3);
-							processRpnMessages(cable, byte1, byte2, byte3, sender);
-							break;
-						case 12:
-							// program change
-							eventListener.onMidiProgramChange(sender, cable, byte1 & 0xf, byte2);
-							break;
-						case 13:
-							// channel pressure
-							eventListener.onMidiChannelAftertouch(sender, cable, byte1 & 0xf, byte2);
-							break;
-						case 14:
-							// pitch bend
-							eventListener.onMidiPitchWheel(sender, cable, byte1 & 0xf, byte2 | (byte3 << 7));
-							break;
-						case 15:
-							// single byte
-							eventListener.onMidiSingleByte(sender, cable, byte1);
-							break;
-						default:
-							// do nothing.
-							break;
 						}
+						break;
+					case 8:
+						eventListener.onMidiNoteOff(sender, cable, byte1 & 0xf, byte2, byte3);
+						break;
+					case 9:
+						if (byte3 == 0x00) {
+							eventListener.onMidiNoteOff(sender, cable, byte1 & 0xf, byte2, byte3);
+						} else {
+							eventListener.onMidiNoteOn(sender, cable, byte1 & 0xf, byte2, byte3);
+						}
+						break;
+					case 10:
+						// poly key press
+						eventListener.onMidiPolyphonicAftertouch(sender, cable, byte1 & 0xf, byte2, byte3);
+						break;
+					case 11:
+						// control change
+						eventListener.onMidiControlChange(sender, cable, byte1 & 0xf, byte2, byte3);
+						processRpnMessages(cable, byte1, byte2, byte3, sender);
+						break;
+					case 12:
+						// program change
+						eventListener.onMidiProgramChange(sender, cable, byte1 & 0xf, byte2);
+						break;
+					case 13:
+						// channel pressure
+						eventListener.onMidiChannelAftertouch(sender, cable, byte1 & 0xf, byte2);
+						break;
+					case 14:
+						// pitch bend
+						eventListener.onMidiPitchWheel(sender, cable, byte1 & 0xf, byte2 | (byte3 << 7));
+						break;
+					case 15:
+						// single byte
+						eventListener.onMidiSingleByte(sender, cable, byte1);
+						break;
+					default:
+						// do nothing.
+						break;
 					}
 				}
 			}
---
+++ MIDIDriver/src/jp/kshoji/driver/midi/util/ReusableByteArrayOutputStream.java
@@ -0,0 +1,45 @@
+package jp.kshoji.driver.midi.util;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * {@link ByteArrayOutputStream} that can reset without memory leak.
+ * 
+ * @author K.Shoji
+ */
+public class ReusableByteArrayOutputStream extends ByteArrayOutputStream {
+	private static final int DEFAULT_BUFFER_LIMIT = 1024;
+	private final byte[] fixedSizeBuffer;
+
+	/**
+	 * Construct instance
+	 * 
+	 * @param size
+	 */
+	public ReusableByteArrayOutputStream(int size) {
+		super(size);
+		fixedSizeBuffer = new byte[size];
+		this.buf = fixedSizeBuffer;
+	}
+
+	/**
+	 * Construct default instance, maximum buffer size is 1024 bytes.
+	 */
+	public ReusableByteArrayOutputStream() {
+		this(DEFAULT_BUFFER_LIMIT);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.io.ByteArrayOutputStream#reset()
+	 */
+	@Override
+	public synchronized void reset() {
+		super.reset();
+		
+		// reset buffer size when the buffer has been extended
+		if (this.buf.length > fixedSizeBuffer.length) {
+			this.buf = fixedSizeBuffer;
+		}
+	}
+}
